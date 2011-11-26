641284339453111d72ae0c94489ba1cc51b222fc
Simon Gingras <sgingras@wantedtech.com>
2011-11-26 14:15:09 -0500

    Added custom leader + Added custom aliases for work related task.

---
+++ wanted/aliases.zsh
@@ -0,0 +1,7 @@
+alias dali='c dali-ui && mvim . && rvm use 1.9.1 && script/server --path=/wa'
+alias edit-dali='c dali-ui && mvim .'
+
+alias ever='c evergood-new-frontier && mvim . && rvm use 1.9.2 && bundle exec rackup'
+alias edit-ever='c evergood-new-frontier && mvim .'
+
+
