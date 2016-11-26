f1bc4c4b13cdf91a441a19d2fc5f72d1d919e83d
Simon Gingras <thegingman@gmail.com>
2016-11-26 17:20:31 -0500

    Cleanup unused files

--- cap/aliases.zsh
+++
@@ -1,7 +0,0 @@
-deploy_prod() {
-  rvm use system && cap prod1 deploy:setup -s milestone="$@" && cap prod1 deploy -s milestone="$@"
-}
-
-deploy_staging() {
-  rvm use system && cap staging deploy:setup -s milestone="$@" && cap staging deploy -s milestone="$@"
-}
--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,9 +1,2 @@
-alias r='rvm use 1.9.1'
-
-alias br='bundle exec rackup'
-alias bs='bundle exec shotgun'
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
+alias bf='bundle exec foreman start web'
+alias migrate='bundle exec rake db:migrate'
